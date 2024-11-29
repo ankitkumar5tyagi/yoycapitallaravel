d47b5b4f1022ea7491c4e65f9b60e5016eeab7b7
Ankit Kumar <kumar1997ankit@gmail.com>
2024-11-29 07:50:57 +0530

    responsive update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             
                 <div class="slideshow-container">
                     <div class="mySlides">
-                        <img class="fade" src="./images/img1.png" style="width:50%; border-radius:50%; opacity: 1;">
+                        <img class="fade" src="./images/img1.png">
                         <div class="text-container">
                             <h1><b>One Place For Many Services!</b></h1>
                             <h2>Govt IDs Facilitation</h2>
@@ -16,7 +16,7 @@
             
                     
                     <div class="mySlides">
-                        <img class="fade" src="./images/img2.png" style="width:50%; border-radius:50%; opacity:1">
+                        <img class="fade" src="./images/img2.png">
                         <div class="text-container">
                             <h1><b>One Place For Many Services!</b></h1>
                             <h2>Documentation & Certificates</h2>
@@ -25,21 +25,21 @@
             
                     
                     <div class="mySlides">
-                        <img class="fade" src="./images/img4.png" style="width:50%; border-radius:50%; opacity:1">
+                        <img class="fade" src="./images/img4.png">
                         <div class="text-container">
                             <h1><b>One Place For Many Services!</b></h1>
                             <h2>GST And Income Tax Services</h2>
                         </div>
                     </div>
                     <div class="mySlides">
-                        <img class="fade" src="./images/img5.jpg" style="width:50%; border-radius:50%; opacity:1">
+                        <img class="fade" src="./images/img5.jpg">
                         <div class="text-container">
                             <h1><b>One Place For Many Services!</b></h1>
                             <h2>Stocks & Mutual Fuds Investment</h2>
                         </div>
                     </div>
                     <div class="mySlides">
-                        <img class="fade" src="./images/img6.png" style="width:50%; border-radius:50%; opacity:1">
+                        <img class="fade" src="./images/img6.png">
                         <div class="text-container">
                             <h1><b>One Place For Many Services!</b></h1>
                             <h2>Courier & Cargo Services</h2>
@@ -47,7 +47,7 @@
                     </div>
 
                     <div class="mySlides">
-                        <img class="fade" src="./images/img3.png" style="width:50%; border-radius:50%; opacity:1">
+                        <img class="fade" src="./images/img3.png">
                         <div class="text-container">
                             <h1><b>One Place For Many Services!</b></h1>
                             <h2>Best Customer Service</h2>
